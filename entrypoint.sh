0853cdf2265d2b2925f2e192fdfcdde0c205d456
Scott Brenner <scott@scottbrenner.me>
2025-05-25 14:20:31 -0700

    Problem matchers

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 set -e
 
+# Copy the matcher to the host system; otherwise "add-matcher" can't find it.
+cp /puppet-lint.json /github/workflow/puppet-lint.json
+echo "::add-matcher::${RUNNER_TEMP}/_github_workflow/puppet-lint.json"
+
 sh -c "puppet-lint $*"
